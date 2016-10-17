fa53928619689649317bd5ab1479999ab9f7b5a7
azardilis <azardilis@gmail.com>
2016-10-17 15:38:24 +0100

    added a full rule to test

--- main.hs
+++ main.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE  QuasiQuotes #-}
 
+import ColouredPetriNets
 import Ext
-import Defs
 
 data Agent = A { a :: Int, b :: Int}
-           | B { c :: Int} deriving (Show)  
+           | B { c :: Int} deriving (Eq, Show)  
 
-r = [rule|A{a=x, b=y}, B{c=x}|]
+
+s :: Multiset Agent
+s = ms [A{a=1, b=2}, B{c=1}]
+
+
+r = [rule|A{a=x, b=y}, B{c=x} --> A{a=x, b=y}, B{c=x+1} @1.0 [True]|]
 
 
