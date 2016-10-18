f833517ec36520b1c6d3e0a90e6b9ca6b70b98a3
azardilis <azardilis@gmail.com>
2016-10-18 16:47:55 +0100

    a model with both time and ext lhs

--- main.hs
+++ main.hs
@@ -1,16 +1,28 @@
 {-# LANGUAGE  QuasiQuotes #-}
+{-# LANGUAGE  TemplateHaskell #-}
 
 import ColouredPetriNets
 import Ext
+import Fluent
+
 
 data Agent = A { a :: Int }
            | B { b :: Int } deriving (Eq, Show)  
 
 
+--- Fluents
+day = between 6 18 (constant True) (constant False)
+
+light = repeatEvery 24 day
+
+temp = when light (constant 21.0) `orElse` constant 19.0
+
+
 state :: Multiset Agent
 state = ms [A{a=1}, A{a=2}, B{b=2}]
 
+$(return [])
 
-r = [rule|A{a=x}, A{a=x+1} --> A{a=x}, A{a=x+1} @1.0 [True] |]
+r = [rule|A{a=x}, A{a=x+1} --> A{a=x}, A{a=x+1} @1.0 [light] |]
 
 
