4c27ca2e85e0af9d96cfa548244c390ce38b5c4f
azardilis <azardilis@gmail.com>
2016-11-11 11:26:46 +0000

    added run methods that allow stopping at specific time

--- src/Chromar/Observables.hs
+++ src/Chromar/Observables.hs
@@ -68,8 +68,8 @@ showTObs (t, obss) = show t ++ " " ++ obssS where
   obssS = unwords (map show obss)
 
 
-writeObs :: (Show a) => FilePath -> [State a] -> [Observable a] -> IO ()
-writeObs fn ss fs = writeFile fn (unlines obsS) where
+writeObs :: (Show a) => FilePath -> [Observable a] -> [State a] -> IO ()
+writeObs fn fs ss = writeFile fn (unlines obsS) where
     obs      = applyObs ss (gens fs)
     obsNames = names fs
     header   = unwords ("time" : obsNames)
@@ -87,4 +87,25 @@ runW :: (Eq a, Show a) => Model a -> Int -> FilePath -> [Observable a] -> IO ()
 runW (Model {rules=rs, initState=s})  n fn obss = do
   rgen <- R.getStdGen
   let traj = take n (simulate rgen rs s)
-  writeObs fn traj obss
+  writeObs fn obss traj
+
+
+runT :: (Eq a, Show a) => Model a -> Time -> [Observable a] -> IO ()
+runT (Model {rules=rs, initState=s}) t obss = do
+  rgen <- R.getStdGen
+  let traj = takeWhile (\s -> getT s < t) (simulate rgen rs s)
+  printObs traj obss
+
+
+runTW :: (Eq a, Show a) => Model a -> Time -> FilePath -> [Observable a] -> IO ()
+runTW (Model {rules=rs, initState=s}) t fn obss = do
+  rgen <- R.getStdGen
+  let traj = takeWhile (\s -> getT s < t) (simulate rgen rs s)
+  writeObs fn obss traj
+
+
+
+
+
+
+
