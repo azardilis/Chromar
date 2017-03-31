01ab2c7f4f3a82df193bc51c53ca228f7128bb3a
azardilis <azardilis@gmail.com>
2017-03-31 10:24:52 +0100

    changed model for new conventions

--- models/plant.hs
+++ models/plant.hs
@@ -1,23 +1,23 @@
 {-# LANGUAGE  QuasiQuotes #-}
+{-# LANGUAGE  TemplateHaskell #-}
 
-import qualified System.Random as R
 import Chromar
 
 --- Agents
-data Token = L { m :: Double }
+data Agent = L { m :: Double }
            | B { c :: Double }
            | R { n :: Int }
            deriving (Eq, Show)
 
-
-isL :: Token -> Bool
+isL :: Agent -> Bool
 isL (L{}) = True
 isL _     = False
 
-isB :: Token -> Bool
+isB :: Agent -> Bool
 isB (B{}) = True
 isB _     = False
 
+$(return [])
 
 --- Rules
 growth       = [rule| L{m=m}, B{c=c} --> L{m=m+1.0}, B{c=c-1.0} @c/m [c-1>0] |]
@@ -30,23 +30,33 @@ s = ms [L{m=0.01}, L{m=0.02}, B{c=0.3}, R{n=2}]
 
 
 --- Observables
-rosMass = Observable { name = "rosetteMass",
-                       gen  = sumM m . select isL } 
+rosMass =
+    Observable
+    { name = "rosetteMass"
+    , gen = sumM m . select isL
+    }
 
+carbon =
+    Observable
+    { name = "carbon"
+    , gen = sumM c . select isB
+    }
 
-carbon = Observable { name = "carbon",
-                      gen  = sumM c . select isB }
+nLeaves =
+    Observable
+    { name = "nLeaves"
+    , gen = countM . select isL
+    }
 
-
-nLeaves = Observable { name = "nLeaves",
-                       gen  = countM . select isL }
-
-
-model = Model { rules     = [growth, assimilation, leafCreation],
-                initState = s }
+model =
+    Model
+    { rules = [growth, assimilation, leafCreation]
+    , initState = s
+    }
 
 --- run
 main :: IO ()
-main = run model nsteps observables where
-  nsteps = 1000
-  observables = [rosMass, carbon, nLeaves]
+main = run model nsteps observables
+  where
+    nsteps = 1000
+    observables = [rosMass, carbon, nLeaves]
