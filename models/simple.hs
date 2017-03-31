21cc94abd2b2a10b576b4d12e6e23959447e58bb
azardilis <azardilis@gmail.com>
2017-03-31 10:25:08 +0100

    changed model for new conventions

--- models/simple.hs
+++ models/simple.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE  QuasiQuotes #-}
+{-# LANGUAGE  TemplateHaskell #-}
 
 import Chromar
 
 -- Agent declarations
-data Token = A { x :: Int }
+data Agent = A { x :: Int }
            deriving (Eq, Show)
 
+$(return [])
 
 -- Rules
 r1 = [rule| A{x=x}, A{x=x'} --> A{x=x+1}, A{x=x'-1} @1.0 [x' > 0] |]
 r2 = [rule| A{x=x}          --> A{x=x}, A{x=0} @1.0 [True] |]
 
-
 --- Initial state
 s = ms [A{x=5}, A{x=5}]
+
+model =
+    Model
+    { rules = [r1, r2]
+    , initState = s
+    }
