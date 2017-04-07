5acdd709c805932fea7175d7244ccdfa637e51ac
azardilis <azardilis@gmail.com>
2017-04-07 17:28:14 +0100

    split into two seperated modules, Chromar exposes the full slow sim and SChromar the fast simple sim

--- src/Chromar.hs
+++ src/Chromar.hs
@@ -1,12 +1,12 @@
 module Chromar (
   module Chromar.Multiset,
-  module Chromar.Sim,
+  module Chromar.Core,
   module Chromar.Observables,
   module Chromar.RuleQuotes,
   module Chromar.Fluent ) where
 
 import Chromar.Multiset
-import Chromar.Sim
+import Chromar.Core
 import Chromar.Observables
 import Chromar.RuleQuotes
 import Chromar.Fluent hiding (Time)
---
+++ src/SChromar.hs
@@ -0,0 +1,12 @@
+module SChromar (
+  module Chromar.Multiset,
+  module Chromar.IMultiset,
+  module Chromar.Sim,
+  module Chromar.SRuleQuotes,
+  module Chromar.Fluent ) where
+
+import Chromar.IMultiset
+import Chromar.Multiset
+import Chromar.Sim
+import Chromar.SRuleQuotes
+import Chromar.Fluent hiding (Time)
