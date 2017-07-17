3c12f188e6a88b6d9b990797309d11d039faea3b
azardilis <azardilis@gmail.com>
2017-07-17 15:29:00 +0100

    added RExprs to the Chromar modules

--- src/Chromar.hs
+++ src/Chromar.hs
@@ -1,12 +1,11 @@
-module Chromar (
-  module Chromar.Multiset,
-  module Chromar.Core,
-  module Chromar.Observables,
-  module Chromar.RuleQuotes,
-  module Chromar.Fluent ) where
+module Chromar
+  ( module Chromar.Multiset
+  , module Chromar.Core
+  , module Chromar.RuleQuotes
+  , module Chromar.RExprs
+  ) where
 
 import Chromar.Multiset
 import Chromar.Core
-import Chromar.Observables
 import Chromar.RuleQuotes
-import Chromar.Fluent hiding (Time)
+import Chromar.RExprs
