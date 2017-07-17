f50e72bff6acce737b857527ab0b49d79fbd3e00
azardilis <azardilis@gmail.com>
2017-07-17 12:02:09 +0100

    added function creation

--- src/Chromar/RExprs.hs
+++ src/Chromar/RExprs.hs
@@ -24,6 +24,36 @@ import Chromar.Core
 
 data ErF a b = ErF { at :: Multiset a -> Time -> b }
 
+zipEr2 :: ErF a b -> ErF a c -> ErF a (b, c)
+zipEr2 e1 e2 =
+    ErF
+    { at = \s t -> (at e1 st, at e2 s t)
+    }
+
+zipEr3 :: ErF a b -> ErF a c -> ErF a d -> ErF a (b, c, d)
+zipEr3 e1 e2 e3 =
+    ErF
+    { at = \s t -> (at e1 st, at e2 s t, at e3 s t)
+    }
+
+zipEr4 :: ErF a b -> ErF a b1 -> ErF a b2 -> ErF a b3 -> ErF a (b, b1, b2, b3)
+zipEr4 e1 e2 e3 e4 =
+    ErF
+    { at = \s t -> (at e1 st, at e2 s t, at e3 s t, at e4 s t)
+    }
+
+zipEr5
+    :: ErF a b
+    -> ErF a b1
+    -> ErF a b2
+    -> ErF a b3
+    -> ErF a b4
+    -> ErF a (b, b1, b2, b3, b4)
+zipEr5 e1 e2 e3 e4 e5 =
+    ErF
+    { at = \s t -> (at e1 st, at e2 s t, at e3 s t, at e4 s t, at e5 s t)
+    }
+
 repeatEvery :: ErF a Time -> ErF a b -> ErF a b
 repeatEvery et er =
     ErF
@@ -174,8 +204,8 @@ timeExpr = do
 
 parensExpr :: Parser Er
 parensExpr = do
-  er <- Text.Parsec.between (Tok.symbol lexer "(") (Tok.symbol lexer ")") parseEr
-  return er
+    er <- Text.Parsec.between (Tok.symbol lexer "(") (Tok.symbol lexer ")") parseEr
+    return er
 
 mkFApp :: Name -> Exp
 mkFApp nm =
@@ -213,17 +243,41 @@ mkLiftExp nms body = LamE args (tExp nms body)
   where
     args = [VarP $ mkName "s", VarP $ mkName "t"]
 
-quoteEr :: Er -> Q Exp
-quoteEr Time = [| time |]
+mkWhenExp :: Exp -> Exp -> Exp -> Exp
+mkWhenExp eb e1 e2 = AppE (AppE (VarE $ mkName "orElse") whenE) e2
+  where
+    whenE = AppE (AppE (VarE $ mkName "when") eb) e1
+
+mkRepeatExp :: Exp -> Exp -> Exp
+mkRepeatExp et e = AppE (AppE (VarE $ mkName "repeatEvery") et) e
+
+mkObsExp :: Nm -> Exp -> Exp -> Exp
+mkObsExp nm combE initE = AppE (AppE selectE combE) initE
+  where
+    selF = "is" ++ nm
+    selectE = AppE (VarE $ mkName "obs") (VarE $ mkName selF)
+
+quoteEr :: Er -> Exp
+quoteEr Time = VarE $ mkName "time"
 quoteEr (HExpr nms e) = AppE (VarE $ mkName "mkEr") (mkLiftExp nms e)
-quoteEr (When er1 er2 er3) = undefined
-quoteEr (Repeat er1 er2) = undefined
-quoteEr (Obs nm er1 er2) = undefined
+quoteEr (When er1 er2 er3) = mkWhenExp (quoteEr er1) (quoteEr er2) (quoteEr er3)
+quoteEr (Repeat er1 er2) = mkRepeatExp (quoteEr er1) (quoteEr er2)
+quoteEr (Obs nm er1 er2) = mkObsExp nm (quoteEr er1) (quoteEr er2)
 
 erQuoter :: String -> Q Exp
-erQuoter = undefined
+erQuoter s = case parse parseEr "er" s of
+  Left err -> error (show err)
+  Right e -> return $ quoteEr e
 ---- parse the quote into Er then create the functions per the semantics
 
+er :: QuasiQuoter
+er =
+    QuasiQuoter
+    { quoteExp = erQuoter
+    , quotePat = undefined
+    , quoteDec = undefined
+    , quoteType = undefined
+    }
 
 ------------- testing
 contents = "repeatEvery {5} (when {$light$ + 1} {5} else {1})"
@@ -231,8 +285,3 @@ contents = "repeatEvery {5} (when {$light$ + 1} {5} else {1})"
 go = case parse parseEr "er" contents of
   (Left err) -> error (show err)
   (Right val) -> val
-  
-  
-  
-
-
