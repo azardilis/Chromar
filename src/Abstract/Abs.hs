3da186deb44fdde0f716b73cc01065721fa27634
azardilis <azardilis@gmail.com>
2017-08-07 17:17:25 +0100

    added comment about translation strategy

--- src/Abstract/Abs.hs
+++ src/Abstract/Abs.hs
@@ -25,3 +25,11 @@ data Chromar e t = Chromar { agentDecls :: [AgentType t]
 instance Bifunctor Chromar where
   bimap ef tf = undefined
 
+{-
+then for every change in the 'inside language' we do bimap ...
+for every change in the representation we need (le'ts take xml as an example)
+
+instance (ToXML e, ToXML t) => ToXML (Chromar e t) where
+   toXML (Chromar{..}) = ...
+-} 
+
