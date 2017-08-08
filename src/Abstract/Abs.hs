e767e5373ff0f1a5335d2b41644c151fe86ec6c2
azardilis <azardilis@gmail.com>
2017-08-08 15:34:35 +0100

    added example representation translation to sexps for a particular choice of E and T

--- src/Abstract/Abs.hs
+++ src/Abstract/Abs.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Abs where
 
 import Data.Bifunctor
-
-type Multiset a = [(a, Int)]
-
-type TEnv t = [(Nm, t)]
+import GHC.Generics
+import Data.Sexp
+import Language.Sexp.Printer
+import Data.Ratio
+import Data.Word8
+import Language.Haskell.Meta.Parse
+import qualified Language.Haskell.TH.Syntax as H
 
 type Nm = String
 
@@ -12,9 +17,13 @@ type AttrName = String
 
 type Var = String
 
+type Multiset a = [(a, Int)]
+
 data AgentType t =
     AgentType Nm
-              [(AttrName, t)]
+              [(AttrName, t)] deriving Generic
+                                       
+instance (Sexpable t) => Sexpable (AgentType t)
 
 instance Functor AgentType where
     fmap f (AgentType nm attrs) =
@@ -25,7 +34,9 @@ instance Functor AgentType where
 
 data RAgent e =
     RAgent Nm
-           [(AttrName, e)]
+           [(AttrName, e)] deriving Generic
+
+instance (Sexpable e) => Sexpable (RAgent e) 
 
 instance Functor RAgent where
     fmap f (RAgent nm attrs) =
@@ -36,14 +47,18 @@ instance Functor RAgent where
 
 data LAgent =
     LAgent Nm
-           [(AttrName, Var)]
+           [(AttrName, Var)] deriving Generic
+
+instance Sexpable LAgent
 
 data ARule e = Rule
     { lhs :: [LAgent]
     , rhs :: [RAgent e]
     , rexpr :: e
     , cexpr :: e
-    }
+    } deriving Generic
+
+instance (Sexpable e) => Sexpable (ARule e)
 
 instance Functor ARule where
     fmap f (Rule {lhs = lhs
@@ -56,15 +71,16 @@ instance Functor ARule where
             [ fmap f rhsAt
             | rhsAt <- rhs ]
         , rexpr = f re
-        , cexpr = f re
+        , cexpr = f ce
         }
 
 data Chromar e t = Chromar
     { agentDecls :: [AgentType t]
     , iState :: Multiset (RAgent e)
     , rules :: [ARule e]
-    }
+    } deriving Generic
 
+instance (Sexpable e, Sexpable t) => Sexpable (Chromar e t)
 
 instance Bifunctor Chromar where
     bimap ef tf (Chromar {agentDecls = adecls
@@ -104,9 +120,6 @@ instance Bifunctor Chromar where
         , rules = rs
         }
                         
-
-
-
 {-
 then for every change in the 'inside language' we do bimap ...
 for every change in the representation we need (le'ts take xml as an example)
@@ -115,3 +128,91 @@ instance (ToXML e, ToXML t) => ToXML (Chromar e t) where
    toXML (Chromar{..}) = ...
 -} 
 
+
+{-
+Example translation where e=H.Exp and t= H.Type
+-}
+
+instance Sexpable H.TyVarBndr
+instance Sexpable H.Stmt
+instance Sexpable H.Type
+instance Sexpable H.Dec
+instance Sexpable H.Body
+instance Sexpable H.Guard
+instance Sexpable H.TyLit
+instance Sexpable H.Clause
+instance Sexpable H.Match
+instance Sexpable H.FunDep
+instance Sexpable H.Pat
+instance Sexpable H.TySynEqn
+instance Sexpable H.Name
+instance Sexpable H.Role
+instance Sexpable H.OccName
+instance Sexpable H.Range
+instance Sexpable H.Pragma
+instance Sexpable H.NameFlavour
+instance Sexpable H.Lit
+instance Sexpable H.Foreign
+instance Sexpable H.RuleMatch
+instance Sexpable H.PkgName
+instance Sexpable H.Fixity
+instance Sexpable H.RuleBndr
+instance Sexpable H.NameSpace
+instance Sexpable H.Safety
+instance Sexpable H.FamFlavour
+instance Sexpable H.Phases
+instance Sexpable H.ModName
+instance Sexpable H.Callconv
+instance Sexpable H.FixityDirection
+instance Sexpable H.Con
+instance Sexpable H.Inline
+instance Sexpable H.AnnTarget
+instance Sexpable H.Strict
+instance Sexpable H.Exp
+
+
+instance (Integral a, Sexpable a) => Sexpable (Ratio a) where
+  toSexp r = List [toSexp $ numerator r, toSexp $ denominator r]
+
+  fromSexp s = undefined
+
+
+instance Sexpable Word8 where
+  toSexp = undefined
+  fromSexp = undefined
+
+parseE :: String -> H.Exp
+parseE s = case parseExp s of
+  Left s -> error s
+  Right e -> e
+
+parseT :: String -> H.Type
+parseT t = case parseType t of
+  Left s -> error s
+  Right t -> t
+
+----create a Chromar(H.Exp, H.T)
+arule = Rule { lhs = [LAgent "A" [("x", "x")]],
+               rhs = [RAgent "A" [("x", parseE "x+1")]],
+               rexpr = parseE "x",
+               cexpr = parseE "True" }
+
+adecl = AgentType "A" [("x", parseT "Int")]
+
+m = Chromar { agentDecls = [adecl],
+              iState = [(RAgent "A" [("x", parseE "1")], 1)],
+              rules = [arule] }
+
+go = printHum (toSexp m)
+
+
+-- instance Sexpable H.Lit where
+--   toSexp (H.CharL c) = toSexp c
+--   toSexp (H.StringL s) = toSexp s
+--   toSexp (H.IntegerL i) = toSexp i
+--   toSexp (H.RationalL r) = List [toSexp $ numerator r, toSexp $ denominator r]
+--   toSexp (H.IntPrimL i) = toSexp i
+--   toSexp (H.WordPrimL i) = toSexp i
+--   toSexp (H.FloatPrimL r) = List [toSexp $ numerator r, toSexp $ denominator r]
+--   toSexp (H.DoublePrimL r) = List [toSexp $ numerator r, toSexp $ denominator r]
+--   toSexp (H.StringPrimL words) = undefined
