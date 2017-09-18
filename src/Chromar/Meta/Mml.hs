b0331a16aa95fe8a69a1a3d96b984e0ef1319928
azardilis <azardilis@gmail.com>
2017-09-18 15:18:00 +0100

    added ToXML instance for RAgent and boolean MExprs

--- src/Chromar/Meta/Mml.hs
+++ src/Chromar/Meta/Mml.hs
@@ -29,10 +29,13 @@ data BinOp
 data NOp
     = Plus
     | Times
+    | And
+    | Or  
     deriving (Show)
 
 data MExp
-    = ConI Integer
+    = ConB Bool
+    | ConI Integer
     | ConD Double
     | Symb MNm
     | UExp UnOp MExp
@@ -40,7 +43,6 @@ data MExp
     | NExp NOp [MExp]
     | MAppE MNm [MExp]       
     deriving (Show)
-
 
 defElem nm cs =
     Element
@@ -102,6 +104,7 @@ class ToXML a where
 
 ---remember to use the mml namespace xmlns="http://www.w3.org/1998/Math/MathML"
 instance ToXML MExp where
+  toXml (ConB b) = defElemAttrs "cn" [("type", "bool")] [textContent $ show b]
   toXml (ConI n) = defElemAttrs "cn" [("type", "integer")] [textContent $ show n]
   toXml (ConD n) = defElemAttrs "cn" [("type", "real")] [textContent $ show n]
   toXml (Symb nm) = defElem "ci" [textContent nm]
@@ -112,10 +115,9 @@ instance ToXML MExp where
   toXml (BExp Pow me1 me2) = mkOpApp "power" [Elem $ toXml me1, Elem $ toXml me2]
   toXml (NExp Plus mes) = mkOpApp "plus" (map (Elem . toXml) mes)
   toXml (NExp Times mes) = mkOpApp "times" (map (Elem . toXml) mes)
+  toXml (NExp And mes) = mkOpApp "and" (map (Elem . toXml) mes)
+  toXml (NExp Or mes) = mkOpApp "or" (map (Elem . toXml) mes)
   toXml (MAppE fnm mes) = mkFnApp fnm (map (Elem . toXml) mes)
-
-instance (ToXML e, ToXML t) => ToXML (Chromar e t) where
-  toXml m = undefined
 
 instance (Show a) => ToXML (AgentType a) where
     toXml (AgentType nm intf) =
@@ -135,14 +137,37 @@ instance ToXML LAgent where
           defElem "attrBind" [Elem $ attrNmElem attrNm, Elem $ attrBindElem v]
       intfElem = defElem "intfLAgent" $ map (Elem . attrElem) intf
 
-instance (ToXML e) => ToXML (RAgent e) where
-  toXml (RAgent nm intf) = undefined
+instance (ToXML e) =>
+         ToXML (RAgent e) where
+    toXml (RAgent nm intf) =
+        defElem "ragent" [Elem $ agentNmElem nm, Elem $ intfElem]
+      where
+        attrExprElem e = defElem "math" [Elem $ toXml e]
+        rattrElem (attrNm, e) =
+            defElem "attrExpr" [Elem $ attrNmElem attrNm, Elem $ attrExprElem e]
+        intfElem = defElem "intfRAgent" $ map (Elem . rattrElem) intf
 
-instance (ToXML e) => ToXML (ARule e) where
-  toXml Rule {lhs = lhs
-             ,rhs = rhs
-             ,rexpr = re
-             ,cexpr = ce} = undefined
+instance (ToXML e) =>
+         ToXML (ARule e) where
+    toXml Rule {lhs = lhs
+               ,rhs = rhs
+               ,rexpr = re
+               ,cexpr = ce} =
+        defElem
+            "rule"
+            [ Elem $ lhsElem 
+            , Elem $ rhsElem 
+            , Elem $ rexprElem 
+            , Elem $ cexprElem
+            ]
+      where
+        lhsElem = defElem "lhs" (map (Elem . toXml) lhs)
+        rhsElem = defElem "rhs" (map (Elem . toXml) rhs)
+        rexprElem = defElem "math" [Elem $ toXml re]
+        cexprElem = defElem "math" [Elem $ toXml ce]
+
+instance (ToXML e, ToXML t) => ToXML (Chromar e t) where
+  toXml m = undefined
 
 viewVar :: Exp -> String
 viewVar (VarE nm) = show nm
@@ -152,8 +177,13 @@ viewT :: Type -> String
 viewT (ConT nm) = show nm
 viewT _ = ""
 
+viewNm :: Name -> String
+viewNm nm = show nm
+
 toMmlExp :: Exp -> MExp
 toMmlExp (LitE (IntegerL n)) = ConI n
+toMmlExp (ConE (viewNm -> "GHC.Types.True")) = ConB True
+toMmlExp (ConE (viewNm -> "GHC.Types.False")) = ConB False
 toMmlExp (VarE nm) = Symb (show nm)
 toMmlExp (AppE (viewVar -> "GHC.Num.abs") e) = UExp Abs (toMmlExp e)
 toMmlExp (AppE (viewVar -> "GHC.Num.negate") e) = UExp Neg (toMmlExp e)
@@ -174,6 +204,14 @@ toMmlExp (InfixE (Just e1) (viewVar -> "GHC.Num.*") (Just e2)) =
     case (toMmlExp e1) of
         NExp Times es -> NExp Times (es ++ [toMmlExp e2])
         me -> NExp Times [me, toMmlExp e2]
+toMmlExp (InfixE (Just e1) (viewVar -> "GHC.Classes.||") (Just e2)) =
+    case (toMmlExp e2) of
+        NExp Or es -> NExp Or (toMmlExp e1: es)
+        me -> NExp Or [toMmlExp e1, me]
+toMmlExp (InfixE (Just e1) (viewVar -> "GHC.Classes.&&") (Just e2)) =
+    case (toMmlExp e2) of
+        NExp And es -> NExp And (toMmlExp e1 : es)
+        me -> NExp And [me, toMmlExp e2]
 toMmlExp (AppE e1 e2) =
     case (toMmlExp e1) of
         MAppE c es -> MAppE c (es ++ [toMmlExp e2])
@@ -184,7 +222,6 @@ toMmlType (viewT -> "GHC.Types.Int") = MInt
 toMmlType (viewT -> "GHC.Types.Double") = MReal
 toMmlType (viewT -> "GHC.Types.Bool") = MBool
 
-
 testAgentType :: IO ()
 testAgentType = mapM_ print $ lines (ppElement (toXml agentt))
   where
@@ -194,6 +231,11 @@ testLAgent :: IO ()
 testLAgent = mapM_ print $ lines (ppElement (toXml ragent))
   where
     ragent = LAgent "A" [("x", "x")]
+
+testRAgent :: IO ()
+testRAgent = mapM_ print $ lines (ppElement (toXml ragent))
+  where
+    ragent = RAgent "A" [("x", NExp Plus [Symb "x", ConI 1])]
 
 testMml :: MExp -> IO ()
 testMml mml = mapM_ print $ lines (ppElement (toXml mml))
